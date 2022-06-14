03a2de29a2249a5257813dde93cff30d36cce8bd
Andriy Golumbovsky <agolumbovsky@gmail.com>
2022-06-13 22:31:58 -0400

    a symbolic change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is MY project!!!
         </a>
       </header>
     </div>
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+
